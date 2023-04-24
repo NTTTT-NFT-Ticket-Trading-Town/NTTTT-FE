4839d46514ff8bf41e84dfb4d0de4f028280d532
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-04-24 22:42:26 +0900

    Feat : add artist slice w/ search api

--- src/store/index.ts
+++ src/store/index.ts
@@ -27,9 +27,13 @@ import {
 } from "react-redux";
 
 import rootReducers from "./reducers";
+import { artistsApi } from "./reducers/artist";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 const store = configureStore({
   reducer: rootReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(artistsApi.middleware),
 });
 
 export type RootState = ReturnType<typeof rootReducers>;
@@ -39,5 +43,7 @@ const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector; // Type I
 const useDispatch = () => useAppDispatch<AppDispatch>(); // Type Inference를 위해 useAppDispatch로 dispatch함수를 만들어준다.
 
 const { dispatch } = store;
+
+setupListeners(dispatch);
 
 export { store, dispatch, useSelector, useDispatch };
---
+++ src/store/reducers/artist/artistTypes.ts
@@ -0,0 +1,8 @@
+export interface ArtistState {
+  id: ArtistIDType;
+  name: string;
+  image_url: string;
+}
+
+export type SearchType = string;
+export type ArtistIDType = number;
