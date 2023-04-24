7d58fa5f3815a04401d93f545f0ed5547a8b610e
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-04-24 21:10:10 +0900

    Feat : add default pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <>
+      <div className="absolute isolate w-full max-w-xl overflow-hidden">
+        <img
+          src="/bg-dark.png"
+          alt=""
+          width={800}
+          height={1131}
+          className="h-screen object-cover"
+        />
+      </div>
+
+      <div className="isolate grid h-full place-content-evenly justify-center pt-10 text-white">
+        <div className="relative grid gap-2">
+          <h1 className="mx-auto text-6xl font-bold italic">NTTTT</h1>
+          <span className="mx-auto text-2xl font-semibold">
+            아이돌 토큰을 모아보세요!
+          </span>
+        </div>
+        <div className="w-full">
+          <Link
+            to="/login"
+            className="block rounded bg-black/50 py-2 text-center transition-all duration-100 hover:bg-black/60 active:scale-95 active:bg-black/80"
+          >
+            로그인
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function Home() {
+export default function Login() {
   return (
     <>
       <main className="relative mx-auto h-full max-w-xl">
@@ -12,6 +12,8 @@ export default function Home() {
             height={1131}
             className="h-screen object-cover"
           />
+
+          <div className="absolute inset-0 bg-black/40"></div>
         </div>
 
         <div className="isolate grid h-full place-content-evenly justify-center pt-10 text-white">
@@ -20,13 +22,17 @@ export default function Home() {
             <span className="mx-auto text-2xl font-semibold">
               아이돌 토큰을 모아보세요!
             </span>
+            <div className="grid gap-4 pt-4">
+              <input className="block w-full rounded bg-neutral-300/30 px-2 py-2" />
+              <input className="block w-full rounded bg-neutral-300/30 px-2 py-2" />
+            </div>
           </div>
-          <div className="w-full">
+          <div>
             <Link
-              to="login"
-              className="block rounded bg-black/50 py-2 text-center transition-all duration-100 hover:bg-black/60 active:scale-95 active:bg-black/80"
+              to="/gotcha"
+              className="block rounded bg-neutral-300/30 py-2 text-center transition-all duration-100 hover:bg-neutral-300/40 active:scale-95 active:bg-neutral-300/50"
             >
-              로그인
+              토큰 모으러 가기
             </Link>
           </div>
         </div>
