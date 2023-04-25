4cfcbce62f133add7bd207f7996224e0982575df
Henry-Hong <hhr8540@naver.com>
2023-04-26 00:04:55 +0900

    Feat : Add PWA with icon.png

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icon.png"],
+      manifest: {
+        name: "NTTTT",
+        short_name: "NTTTT",
+        description: "Non-Fungible-Team-NFT-Project",
+        start_url: "/",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
