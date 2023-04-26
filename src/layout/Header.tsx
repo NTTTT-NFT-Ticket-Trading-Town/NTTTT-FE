7b69143e3980df12766e2de37130243f569de2c4
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-04-26 14:44:51 +0900

    Refactor : Gotcha page

---
+++ src/layout/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="mx-auto max-w-xl">
+      <nav className="flex items-center font-extrabold italic">
+        <div className="grow py-4 sm:py-6">
+          <div className="text-center text-lg text-neutral-400 sm:text-2xl">
+            NTTTT
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
