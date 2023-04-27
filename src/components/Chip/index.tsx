e377b94712774db1e1e45b9ba1a2b211d9b5cda2
Henry-Hong <hhr8540@naver.com>
2023-04-27 21:55:28 +0900

    Fix : chage basicStyle of Chip

--- src/components/Chip/index.tsx
+++ src/components/Chip/index.tsx
@@ -5,6 +5,7 @@ export default function Chip(props: ChipPropsInterface) {
   const { label, enabled = false } = props;
 
   // styles
+  // DEVELOP: tailwind snippet을 볼 수 없을까?
   const basicStyle = [
     "min-w-[50px]",
     "inline-block",
@@ -13,22 +14,13 @@ export default function Chip(props: ChipPropsInterface) {
     "rounded-full",
     "px-4",
     "py-1",
+    "mx-1",
   ].join(" ");
-
-  // hooks
-  // TODO: make it using useTheme hook & apply the color to the tailwind.config as shown in the Figma
-  const theme = {
-    color: {
-      primary: "bg-primary-500",
-      secondary: "bg-secondary-500",
-      neutral: "bg-neutral-500",
-    },
-  };
 
   return (
     <div
       className={`${
-        enabled ? theme.color.primary : theme.color.neutral
+        enabled ? "bg-primary text-white" : "bg-gray-1"
       } ${basicStyle} `}
     >
       {label}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
+      // you can use it like this: bg-primary, bg-gray-1
       primary: "#9E00FF",
       black: "#000000",
       white: "#ffffff",
