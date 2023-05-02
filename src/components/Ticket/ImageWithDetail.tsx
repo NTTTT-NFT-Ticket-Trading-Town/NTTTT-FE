f7795da06580c658cf0ed58549ce52cf56568249
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-05-02 21:57:56 +0900

    Fix : flip 애니메이션을 상위 div로 이동

--- src/components/Ticket/ImageWithDetail.tsx
+++ src/components/Ticket/ImageWithDetail.tsx
@@ -15,59 +15,57 @@ export default function ImageWithDetail(
       className="relative cursor-pointer"
       style={{
         perspective: "800px",
-        transformStyle: "preserve-3d",
       }}
       onTap={(_) => {
         setShowDetail((prev) => !prev);
       }}
     >
       <motion.div
-        className="relative"
-        style={{
-          aspectRatio: gacha.image.ratio,
-          width: "100%",
-        }}
-        initial={false}
-        transition={{
-          duration: animationDuration,
-          ease: easingFunction,
+        style={{ transformStyle: "preserve-3d" }}
+        initial={{
+          rotateY: 0,
         }}
         animate={{
           rotateY: showDetail ? 180 : 0,
         }}
-      >
-        <img
-          draggable={false}
-          src={gacha.image.url}
-          alt=""
-          className="absolute inset-0 -z-10 scale-105 animate-pulse rounded bg-gray-300 object-cover opacity-80 blur-xl"
-        />
-        <img
-          draggable={false}
-          src={gacha.image.url}
-          alt=""
-          className="absolute inset-0 rounded bg-gray-300 object-cover"
-          style={{
-            aspectRatio: gacha.image.ratio,
-          }}
-        />
-      </motion.div>
-      <motion.div
-        initial={false}
         transition={{
           duration: animationDuration,
           ease: easingFunction,
-          backfaceVisibility: "hidden",
         }}
-        animate={{
-          rotateY: showDetail ? 0 : -180,
-          opacity: showDetail ? 1 : 0,
-          translateZ: 1,
-        }}
-        className="absolute top-0 h-full w-full overflow-y-scroll rounded-md bg-black/60 p-8 text-xl text-white"
       >
-        <h3 className="mb-4 text-3xl font-bold">상세 설명</h3>
-        <p>{gacha.description}</p>
+        <div
+          className="relative"
+          style={{
+            aspectRatio: gacha.image.ratio,
+            width: "100%",
+          }}
+        >
+          <img
+            draggable={false}
+            src={gacha.image.url}
+            alt=""
+            className="absolute inset-0 -z-10 scale-105 animate-pulse rounded bg-gray-300 object-cover opacity-80 blur-xl"
+          />
+          <img
+            draggable={false}
+            src={gacha.image.url}
+            alt=""
+            className="absolute inset-0 rounded bg-gray-300 object-cover"
+            style={{
+              aspectRatio: gacha.image.ratio,
+            }}
+          />
+        </div>
+        <div
+          style={{
+            rotate: "y 180deg",
+            backfaceVisibility: "hidden",
+          }}
+          className="absolute top-0 h-full w-full overflow-y-scroll rounded-md bg-black/60 p-8 text-xl text-white"
+        >
+          <h3 className="mb-4 text-3xl font-bold">상세 설명</h3>
+          <p>{gacha.description}</p>
+        </div>
       </motion.div>
     </motion.div>
   );
