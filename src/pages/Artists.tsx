d36865fa1a4dafe3339845a827a0e0c2c3816c47
Henry-Hong <hhr8540@naver.com>
2023-05-05 15:49:09 +0900

    Feat : Artists 페이지에 Finder 컴포넌트추가

--- src/pages/Artists.tsx
+++ src/pages/Artists.tsx
@@ -1,44 +1,66 @@
+import { useState } from "react";
 import Chip from "../components/Chip";
+import Finder from "../components/Finder";
 import MiniTicket from "../components/MiniTicket";
 import Header from "../layout/Header";
 
 // TODO: store에서 처리.
-const imgs = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-RF1_iqBEPjN6gnJb82XeEJbVkF56RZySLw&usqp=CAU", "https://ojsfile.ohmynews.com/STD_IMG_FILE/2022/1217/IE003091219_STD.jpg", "https://isplus.com/data/isp/image/2023/02/18/isp20230218000168.600x.8.jpg", "https://cdn.sisamagazine.co.kr/news/photo/202304/487355_495347_828.png", "https://cdn.mhnse.com/news/photo/202212/161460_156782_2326.jpg", "https://img1.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202206/24/starnews/20220624181914689zfdx.jpg", "https://img.hankyung.com/photo/202112/03.28304862.1.jpg"]
+const imgs = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-RF1_iqBEPjN6gnJb82XeEJbVkF56RZySLw&usqp=CAU",
+  "https://ojsfile.ohmynews.com/STD_IMG_FILE/2022/1217/IE003091219_STD.jpg",
+  "https://isplus.com/data/isp/image/2023/02/18/isp20230218000168.600x.8.jpg",
+  "https://cdn.sisamagazine.co.kr/news/photo/202304/487355_495347_828.png",
+  "https://cdn.mhnse.com/news/photo/202212/161460_156782_2326.jpg",
+  "https://img1.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202206/24/starnews/20220624181914689zfdx.jpg",
+  "https://img.hankyung.com/photo/202112/03.28304862.1.jpg",
+];
 const img_len = imgs.length;
 const artists = new Array(7).fill(null).map((e, idx) => {
-  console.log(imgs[idx % img_len])
   return {
     title: `아티스트 ${idx + 1}`,
-    img_url: imgs[idx % img_len]
-  }
-})
+    img_url: imgs[idx % img_len],
+  };
+});
 
 export default function Artists() {
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  // handlers
+  const handleOnChange = (value: string) => {
+    setSearchValue(value);
+  };
+
   return (
     <>
-    <Header />
-    <main className="relative mx-auto mb-4 w-full max-w-xl px-4">
-      <div className="flex flex-col items-center justify-center">
-      {/* article - header */}
-        <div className="font-bold text-lg">아티스트를 선택해주세요</div>
-        <div className="text-sm">선택하신 아티스트의 NFT토큰을 확인하세요!</div>
+      <Header />
+      <main className="relative mx-auto mb-4 w-full max-w-xl px-4">
+        <div className="flex flex-col items-center justify-center">
+          {/* article - header */}
+          <div className="text-lg font-bold">아티스트를 선택해주세요</div>
+          <div className="text-sm">
+            선택하신 아티스트의 NFT토큰을 확인하세요!
+          </div>
+          {/* article - categorory */}
+          {/* TODO: scroll-bar 없애기 */}
+          <div className="my-3 flex h-9 w-full flex-row overflow-scroll">
+            <Finder onChange={handleOnChange} />
+            {/* {new Array(5).fill(null).map((_, idx) => (
+              <Chip label={`카테고리${idx + 1}`} enabled={false} />
+            ))} */}
+          </div>
 
-      {/* article - categorory */}  
-      {/* TODO: scroll-bar 없애기 */}
-      <div className="flex flex-row w-full overflow-scroll">
-        {new Array(10).fill(null).map((_, idx) => <Chip label={`카테고리${idx+1}`} enabled={false} />)}
-      </div>
-      {/* article - body */}
-      <div className="grid grid-cols-3 gap-3">
-        {artists.map((artist) => {
-          return <MiniTicket title={artist.title} img_url={artist.img_url} />
-        })}
-      </div>
-
-      {/* article - footer */}
-      <div>푸터</div>
-      </div>
-    </main>
+          {/* article - body */}
+          <div className="grid grid-cols-3 gap-3">
+            {artists.map((artist) => {
+              return (
+                <MiniTicket title={artist.title} img_url={artist.img_url} />
+              );
+            })}
+          </div>
+          {/* article - footer */}
+          <div>푸터</div>
+        </div>
+      </main>
     </>
   );
 }
