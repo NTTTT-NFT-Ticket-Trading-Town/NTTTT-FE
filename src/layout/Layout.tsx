e327bb5d7aa3d2da88e0f25dbc720d2c7b7b8b1c
Henry-Hong <hhr8540@naver.com>
2023-05-17 20:38:49 +0900

    Feat : page transition added

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,25 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation, useOutlet } from "react-router";
+import { AnimatePresence, motion } from "framer-motion";
+import AnimatedOutlet from "./AnimatedOutlet";
 
 export default function Layout() {
+  const location = useLocation();
   return (
-    <div className="relative mx-auto flex min-h-full w-full max-w-xl grow flex-col">
-      <Outlet />
-    </div>
+    <AnimatePresence mode="popLayout">
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -100 }}
+        transition={{ type: "spring", stiffness: 100 }}
+        className="relative mx-auto flex min-h-full w-full max-w-xl grow flex-col"
+      >
+        <AnimatedOutlet />
+      </motion.div>
+    </AnimatePresence>
   );
 }
+
+// <div className="relative mx-auto flex min-h-full w-full max-w-xl grow flex-col">
+// <Outlet />
+// </div>
