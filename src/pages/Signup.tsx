6a816367e4a1ff1d6090a964af87c5ea3d55a9ef
Henry-Hong <hhr8540@naver.com>
2023-05-23 12:35:14 +0900

    Feat : signup page ui 완성

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,118 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { setToken, useLoginMutation } from "../store/reducers/user";
+import { motion } from "framer-motion";
+import metamask from "../assets/metamask.svg";
+
+export default function Signup() {
+  const [login, { data, error, isLoading, isSuccess, status }] =
+    useLoginMutation();
+  const response = data;
+  const errorRes = error as any;
+  const dispatch = useDispatch();
+  const [nickname, setNickname] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNo, setPhoneNo] = useState("");
+  const [walletAddress, setWalletAddress] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSuccess && response) {
+      dispatch(setToken(response.data));
+      navigate("/gacha");
+    }
+  }, [isSuccess, response]);
+
+  const onClick = () => {
+    login({ nickname, password });
+  };
+
+  return (
+    <>
+      <main className="relative flex w-full max-w-xl grow">
+        <div className="absolute isolate w-full max-w-xl overflow-hidden">
+          <img
+            src="/bg-dark.png"
+            alt=""
+            width={800}
+            height={1131}
+            className="h-screen object-cover"
+          />
+
+          <div className="absolute inset-0 bg-black/40"></div>
+        </div>
+
+        <div className="isolate grid w-full grow place-content-evenly justify-center pt-5 text-white ">
+          <div className="relative  grid w-full gap-2">
+            <div className="mb-10 flex flex-col justify-center">
+              <h1 className=" mx-auto px-36 text-2xl font-bold italic">
+                NTTTT
+              </h1>
+              <span className="mx-auto text-2xl font-semibold">회원가입</span>
+            </div>
+            <div className=" grid w-full gap-4 pt-4">
+              <div className="-mb-2 text-gray-1">아이디</div>
+              <input
+                onChange={(e) => setNickname(e.target.value)}
+                value={nickname}
+                className="block w-full rounded bg-neutral-300/30 px-2 py-2"
+              />
+              <div className="-mb-2 text-gray-1">비밀번호</div>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                className="block w-full rounded bg-neutral-300/30 px-2 py-2"
+              />
+              <div className="-mb-2 text-gray-1">전화번호</div>
+              <input
+                onChange={(e) => setPhoneNo(e.target.value)}
+                value={phoneNo}
+                className="block w-full rounded bg-neutral-300/30 px-2 py-2"
+              />
+
+              <div className="-mb-2 mt-12 text-gray-1">지갑주소</div>
+              <div className="relative">
+                <input
+                  onChange={(e) => setWalletAddress(e.target.value)}
+                  value={walletAddress}
+                  className="absolute block w-full rounded bg-neutral-300/30 px-2 py-2 pr-16"
+                />
+                <div className="absolute right-0 mx-auto block h-10 w-14 cursor-pointer rounded bg-white bg-opacity-25 py-2 text-center">
+                  <img
+                    className="mx-auto transition hover:scale-110"
+                    src={metamask}
+                  />
+                </div>
+              </div>
+              {/* <img
+                style={{ width: 25, marginRight: "0.5rem" }}
+                src={metamask}
+              /> */}
+              <div className="text-center text-base text-red-500">
+                {status === "rejected" && (
+                  <motion.span
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                  >
+                    {errorRes.data.result.message}
+                  </motion.span>
+                )}
+              </div>
+            </div>
+          </div>
+          <div>
+            <button
+              onClick={onClick}
+              className=" inline-flex w-full items-center justify-center rounded bg-neutral-300/30 py-2 text-center transition-all duration-100 hover:bg-neutral-300/40 active:scale-95 active:bg-neutral-300/50"
+            >
+              회원가입
+              {isLoading && " 중..."}
+              {isSuccess && " 성공!"}
+            </button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
