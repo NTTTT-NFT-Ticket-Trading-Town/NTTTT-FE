b841de9cdd9d38cdc3ba22d7a004f20b9d44a7b3
Henry-Hong <hhr8540@naver.com>
2023-05-24 19:25:31 +0900

    Feat : 아티스트 페이지 미니티켓 삭제시 슬라이딩 애니메이션 추가

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,18 @@
+import { HTMLAttributes } from "react";
 import { useNavigate } from "react-router";
+
+interface ButtonPropsInterface extends HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+  to?: string;
+  onClick?: () => void;
+}
 
 export default function Button({
   children,
   to = "",
   onClick,
-}: {
-  children: React.ReactNode;
-  to?: string;
-  onClick?: () => void;
-}) {
+  ...props
+}: ButtonPropsInterface) {
   const navigate = useNavigate();
 
   const handleOnClick = () => {
@@ -19,13 +23,15 @@ export default function Button({
   };
 
   return (
-    <div className="align-center flex flex-col items-center justify-center p-1">
-      <button
-        onClick={handleOnClick}
-        className="h-full self-end rounded bg-purple-600 px-14 py-1 text-xl font-bold text-purple-100 transition-all duration-100 hover:bg-purple-500 active:scale-95 active:bg-purple-700"
-      >
-        {children}
-      </button>
+    <div {...props}>
+      <div className="align-center flex flex-col items-center justify-center p-1">
+        <button
+          onClick={handleOnClick}
+          className="h-full self-end rounded bg-purple-600 px-14 py-1 text-xl font-bold text-purple-100 transition-all duration-100 hover:bg-purple-500 active:scale-95 active:bg-purple-700"
+        >
+          {children}
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/Artists.tsx
+++ src/pages/Artists.tsx
@@ -55,66 +55,71 @@ export default function Artists() {
   return (
     <>
       <Header />
-      <main className="relative mx-auto mb-4 w-full max-w-xl px-4">
-        <div className="flex flex-col items-center justify-center">
-          {/* article - header */}
-          <div className="text-lg font-bold">아티스트를 선택해주세요</div>
-          <div className="text-sm">
-            선택하신 아티스트의 NFT토큰을 확인하세요!
-          </div>
-          {/* article - categorory */}
-          <div className="my-3 flex h-9 w-full flex-row overflow-scroll scrollbar-hide">
-            <Finder />
-            {groups.map((group) => (
-              <Chip
-                key={group}
-                handleClick={() => dispatch(toggleFavoriteGroups(group))}
-                label={group}
-                active={selectedGroups.includes(group)}
-              />
-            ))}
-          </div>
+      <main className="relative mx-auto mb-4 w-full max-w-xl px-4 flex flex-col flex-grow">
+        {/* article - header */}
+        <div className=" text-center text-lg font-bold">
+          아티스트를 선택해주세요
+        </div>
+        <div className="text-center text-sm">
+          선택하신 아티스트의 NFT토큰을 확인하세요!
+        </div>
+        {/* article - categorory */}
+        <div className="my-3 flex h-9 w-full flex-row overflow-scroll scrollbar-hide">
+          <Finder />
+          {groups.map((group) => (
+            <Chip
+              key={group}
+              handleClick={() => dispatch(toggleFavoriteGroups(group))}
+              label={group}
+              active={selectedGroups.includes(group)}
+            />
+          ))}
+        </div>
 
-          {/* article - body */}
-          <div className="grid gap-3 xs:grid-cols-3 xs:gap-0 sm:grid-cols-4 sm:gap-2">
-            {artists
-              .filter((artist) => artist.name.includes(search))
-              .filter((artist) => {
-                if (selectedGroups.length === 0) return true;
-                return selectedGroups.includes(artist.group);
-              })
-              .map((artist, idx) => {
-                return (
-                  <MiniTicket
-                    key={artist.id}
-                    delay={idx}
-                    onClick={() => dispatch(toggleFavoriteArtist(artist))}
-                    clicked={!!selectedArtists.find((e) => e.id === artist.id)}
-                    title={artist.name}
-                    img_url={artist.img_url}
-                  />
-                );
-              })}
-          </div>
+        {/* article - body */}
+        <div className="grid gap-3 xs:grid-cols-3 xs:gap-0 sm:grid-cols-4 sm:gap-2">
+          {artists
+            .filter((artist) => artist.name.includes(search))
+            .filter((artist) => {
+              if (selectedGroups.length === 0) return true;
+              return selectedGroups.includes(artist.group);
+            })
+            .map((artist, idx) => {
+              return (
+                <MiniTicket
+                  key={artist.id}
+                  delay={idx}
+                  onClick={() => dispatch(toggleFavoriteArtist(artist))}
+                  clicked={!!selectedArtists.find((e) => e.id === artist.id)}
+                  title={artist.name}
+                  img_url={artist.img_url}
+                />
+              );
+            })}
+        </div>
 
-          {/* article - footer */}
-          <div className=" fixed bottom-0 flex w-full max-w-[36rem] items-center justify-between bg-opacity-10 bg-gradient-to-t from-white to-transparent p-4">
-            <div className="flex max-w-[calc(100%-200px)] gap-3 overflow-x-scroll scrollbar-hide">
-              <AnimatePresence>
-                {selectedArtists.map((artist) => (
-                  <MiniImage
-                    key={artist.id}
-                    src={artist.img_url}
-                    alt="image"
-                    onClick={() => {
-                      dispatch(toggleFavoriteArtist(artist));
-                    }}
-                  />
-                ))}
-              </AnimatePresence>
-            </div>
-            <Button onClick={handleOnClickSave}>저장</Button>
+        {/* article - footer */}
+        <div className="fixed left-0 bottom-0 w-full max-w-[36rem] bg-opacity-10 bg-gradient-to-t from-white to-transparent">
+          <div className="ml-4 mb-4 flex max-w-[calc(100%-200px)] gap-3 overflow-x-scroll scrollbar-hide">
+            <AnimatePresence>
+              {selectedArtists.map((artist) => (
+                <MiniImage
+                  key={artist.img_url}
+                  src={artist.img_url}
+                  alt="image"
+                  onClick={() => {
+                    dispatch(toggleFavoriteArtist(artist));
+                  }}
+                />
+              ))}
+            </AnimatePresence>
           </div>
+          <Button
+            className="absolute bottom-4 right-4"
+            onClick={handleOnClickSave}
+          >
+            저장
+          </Button>
         </div>
       </main>
     </>
