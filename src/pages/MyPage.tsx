1dee0406e054d749155f9b1b0cb21a5962a80fef
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-05-28 22:19:28 +0900

    Refactor: always show logout button

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -21,53 +21,13 @@ export default function MyPage() {
 }
 
 function MyPageContent() {
-  const { data: userData } = useDetailQuery();
-  const { data, isLoading, error } = useGetMyCollectionQuery();
-  const someCategory = data?.data?.category_list.slice(0, 3) || [];
   const navigate = useNavigate();
-
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
-  if (error) {
-    const errorData = (error as any)
-      .data as ServerResponseInterface<GachaInterface>;
-    return <ErrorContent errorMessage={errorData?.result?.message} />;
-  }
-
-  if (!userData || !data || !data.data || !data.data.gacha_list) {
-    return <ErrorContent errorMessage="데이터가 없습니다." />;
-  }
-
-  const userDetail = userData.data;
 
   return (
     <>
       <main className="relative mx-auto mb-4 w-full max-w-xl grow px-4">
         <header className="flex items-center justify-between gap-2 pt-4">
-          <div className="flex flex-row items-center gap-2">
-            <div className="aspect-square w-20 overflow-hidden rounded-full border-4">
-              <img
-                src={"bg-bright.png"}
-                alt=""
-                className="w-full object-cover"
-              />
-            </div>
-            <div className="flex flex-col">
-              <h3 className="font-smei text-xl">{userDetail.nickname}</h3>
-              <div className="flex gap-2">
-                {someCategory.length > 0 ? (
-                  someCategory.map((tag) => (
-                    <div key={tag.name + tag.group} className="text-gray-400">
-                      #{tag.name}
-                    </div>
-                  ))
-                ) : (
-                  <div className="text-gray-400">아티스트를 선택해주세요!</div>
-                )}
-              </div>
-            </div>
-          </div>
+          <Profile />
 
           <div className="flex flex-col gap-2">
             <Link to="/artists" className="rounded bg-gray-300 p-2 text-xs">
@@ -85,32 +45,87 @@ function MyPageContent() {
           </div>
         </header>
         <section className="gap-100 relative flex flex-col pt-4 sm:pt-8">
-          {data?.data.gacha_list.length !== 0 ? (
-            <>
-              {data.data.gacha_list.map((gacha, index) => {
-                console.log(gacha);
-                return <CollectionTicket key={index} gacha={gacha} />;
-              })}
-              <div key="invisible-padding" className="h-[50vh] w-full"></div>
-            </>
-          ) : (
-            <div className="flex flex-col items-center gap-2">
-              <div className="text-center text-gray-500">
-                보유한 티켓이 없습니다.
-              </div>
-              <Link
-                className="rounded bg-purple-600 px-4 py-2 text-white"
-                to="/gacha"
-              >
-                구매하러 가기
-              </Link>
-            </div>
-          )}
+          <CollectionList />
         </section>
       </main>
     </>
   );
 }
+
+const Profile = () => {
+  const { data: userData } = useDetailQuery();
+  const { data } = useGetMyCollectionQuery();
+
+  const someCategory = data?.data?.category_list.slice(0, 3) || [];
+
+  const userDetail = userData?.data;
+
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <div className="aspect-square w-20 overflow-hidden rounded-full border-4">
+        <img src={"bg-bright.png"} alt="" className="w-full object-cover" />
+      </div>
+      <div className="flex flex-col">
+        <h3 className="font-smei text-xl">
+          {userDetail?.nickname ?? "데이터 없음"}
+        </h3>
+        <div className="flex gap-2">
+          {someCategory.length > 0 ? (
+            someCategory.map((tag) => (
+              <div key={tag.name + tag.group} className="text-gray-400">
+                #{tag.name}
+              </div>
+            ))
+          ) : (
+            <div className="text-gray-400">아티스트를 선택해주세요!</div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CollectionList = () => {
+  const { data, isLoading, error } = useGetMyCollectionQuery();
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+  if (error) {
+    const errorData = (error as any)
+      .data as ServerResponseInterface<GachaInterface>;
+    return <ErrorContent errorMessage={errorData?.result?.message} />;
+  }
+
+  if (!data || !data.data || !data.data.gacha_list) {
+    return <ErrorContent errorMessage="데이터가 없습니다." />;
+  }
+
+  return (
+    <>
+      {data?.data.gacha_list.length !== 0 ? (
+        <>
+          {data.data.gacha_list.map((gacha, index) => {
+            return <CollectionTicket key={index} gacha={gacha} />;
+          })}
+          <div key="invisible-padding" className="h-[50vh] w-full"></div>
+        </>
+      ) : (
+        <div className="flex flex-col items-center gap-2">
+          <div className="text-center text-gray-500">
+            보유한 티켓이 없습니다.
+          </div>
+          <Link
+            className="rounded bg-purple-600 px-4 py-2 text-white"
+            to="/gacha"
+          >
+            구매하러 가기
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
 
 const CollectionTicket = ({ gacha }: { gacha: GachaInterface }) => {
   if (!gacha) return null;
