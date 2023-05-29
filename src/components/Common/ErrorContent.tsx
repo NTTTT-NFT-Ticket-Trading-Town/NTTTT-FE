c638eb194249463127a8f4fb9f230482e992220b
Henry-Hong <hhr8540@naver.com>
2023-05-29 10:51:59 +0900

    Fix : UI/UX 개선
    - 만약 에러 메시지에서 유저의 아티스트 선택정보가 없다면, 아티스트 선택 버튼으로 변경
    - 에러컨텐츠 글자 크기 변경

--- src/components/Common/ErrorContent.tsx
+++ src/components/Common/ErrorContent.tsx
@@ -29,20 +29,22 @@ export default function ErrorContent({
       <div className="place-center grid text-9xl">☢️</div>
       <div className="text-center text-6xl font-bold">Error!</div>
       <div className="text-center text-2xl">
-        <p className="text-sm">
+        <p className="text-md">
           {errorMessage ?? "예기치 못한 에러가 발생했습니다."}
         </p>
-        <p className="text-sm">다시 시도해주세요.</p>
+        <p className="text-md">다시 시도해주세요.</p>
       </div>
       <div className="relative flex h-min w-full flex-col gap-4 p-4 sm:p-8">
         <div className="relative">
           <button
             onClick={() => {
-              navigate(0);
+              errorMessage?.includes("선택")
+                ? navigate("/artists")
+                : navigate(0);
             }}
             className="w-full self-end rounded bg-purple-600 py-2 text-center text-xl font-bold text-purple-100 transition-all duration-100 hover:bg-purple-500 active:scale-95 active:bg-purple-700 sm:py-4 sm:text-2xl"
           >
-            새로고침
+            {errorMessage?.includes("선택") ? "아티스트 선택하기" : "새로고침"}
           </button>
           <div className="absolute -inset-2 -z-10 animate-pulse bg-purple-300 blur-xl sm:-inset-4"></div>
         </div>
