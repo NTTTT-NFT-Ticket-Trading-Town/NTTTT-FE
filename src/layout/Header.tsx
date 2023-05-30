fd7dabdd55e10dfd950bf56b8da6ab7a4cbf39fa
Henry-Hong <hhr8540@naver.com>
2023-05-30 15:54:15 +0900

    Hotfix : 마이페이지 문구삭제

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -59,7 +59,7 @@ export default function Header() {
             {isMypage && <Ping />}
           </Link>
           <Link to="/mypage">
-            <div className="relative -mt-1 grid place-items-center text-gray-500">
+            <div className="relative grid place-items-center text-gray-500">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -74,7 +74,6 @@ export default function Header() {
                   d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
                 />
               </svg>
-              <p className="absolute -bottom-4 w-14 text-xs">마이페이지</p>
             </div>
           </Link>
         </div>
